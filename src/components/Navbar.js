438c3a0a15962f1dfc7f75eea7d9b978304d9856
alexthezhang228 <110424337+alexthezhang228@users.noreply.github.com>
2023-04-26 14:46:43 +0200

    add rel

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@
  * @Author: alexthezhang228 110424337+alexthezhang228@users.noreply.github.com
  * @Date: 2023-03-31 11:13:23
  * @LastEditors: alexthezhang228 110424337+alexthezhang228@users.noreply.github.com
- * @LastEditTime: 2023-04-26 14:01:29
+ * @LastEditTime: 2023-04-26 14:37:32
  * @FilePath: /backroads/myapp/src/components/Navbar.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -36,7 +36,7 @@ const Navbar=()=>{
             const {id,href,icon}=link
             return(
               <li key={id}> 
-                <a href={href} target="_blank" className="nav-icon" >
+                <a href={href} target="_blank" rel="noreferrer" className="nav-icon" >
                   <i className={icon}></i>
                 </a>
               </li>
